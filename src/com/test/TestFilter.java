f38c476d3a0b915dfc880727e0f8c7ff198e3457
Caleb Larsen <caleb.larsen@rentvision.com>
2024-12-04 09:22:40 -0600

    Add catch-all @WebFilter for testing.

---
+++ src/com/test/TestFilter.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class TestFilter implements Filter {
+    private static final Logger LOGGER = Logger.getLogger(TestFilter.class.getName());
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        LOGGER.info("doFilter");
+        chain.doFilter(request, response);
+    }
+}
