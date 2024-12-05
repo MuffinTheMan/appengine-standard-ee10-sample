b8e05362756eeca8c58c9ffb9c080e764057e5b7
Caleb Larsen <caleb.larsen@rentvision.com>
2024-12-05 15:21:35 -0600

    Progress commit for backup.

--- src/com/test/TestFilter.java
+++ src/com/test/TestFilter.java
@@ -5,13 +5,17 @@ import java.util.logging.Logger;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 
-@WebFilter("/*")
+@WebFilter("*")
 public class TestFilter implements Filter {
     private static final Logger LOGGER = Logger.getLogger(TestFilter.class.getName());
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         LOGGER.info("doFilter");
+        FileInspector.inspectDirectory(".");
+        FileInspector.inspectDirectory("/workspace");
+        FileInspector.inspectDirectory("/workspace/WEB-INF");
+        FileInspector.inspectDirectory("/workspace/java_runtime");
         chain.doFilter(request, response);
     }
 }
